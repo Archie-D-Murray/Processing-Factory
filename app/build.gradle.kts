e498f76f9545657f2cd88a854711558090ab4e80
Archie-Murray <76486169+Archie-Murray@users.noreply.github.com>
2024-04-01 16:48:04 +0100

    feat: Created release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,3 +34,21 @@ application {
     // Define the main class for the application.
     mainClass.set("factory.Factory")
 }
+
+tasks.withType<Jar> {
+    // Otherwise you'll get a "No main manifest attribute" error
+    manifest {
+        attributes["Main-Class"] = "factory.Factory"
+    }
+    
+    // To avoid the duplicate handling strategy error
+    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+
+    // To add all of the dependencies otherwise a "NoClassDefFoundError" error
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+    from({
+        configurations.runtimeClasspath.get().filter { it.name.endsWith("jar") }.map { zipTree(it) }
+    })
+}
